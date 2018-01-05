bbbc6b06f003838b366a4b87fcde1f8d50f420ca
Niam Islam <niamislam99@gmail.com>
2018-01-05 11:53:34 +0000

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "test.hpp"
+#include "test2.hpp"
+
+int main() {
+     function();
+     function2();
+     return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int function2(){
+    std::cout << "Bye!!!!" << std::endl;
+                            return 0;
+}
