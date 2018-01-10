0e629f1f8ee063689ec41c27d5caf40103c208d3
Niam Islam <niamislam99@gmail.com>
2018-01-10 10:21:31 +0000

    add project wt

--- addwt/HelloApplication.cpp
+++ addwt/HelloApplication.cpp
@@ -1,18 +1,19 @@
 #include "HelloApplication.hpp"
 
+
+
 HelloApplication::HelloApplication(const Wt::WEnvironment& env)
   : WApplication(env)
 {
   setTitle("Hello world");                            // application title
 
-  root()->addWidget(Wt::cpp14::make_unique<Wt::WText>("What is your name: ")); // show some text
+  root()->addWidget(Wt::cpp14::make_unique<Wt::WText>("Enter numbers you wish to add:  ")); // show some text
 
   nameEdit_ = root()->addWidget(Wt::cpp14::make_unique<Wt::WLineEdit>()); // allow text input
   nameEdit_->setFocus();                              // give focus
-
-  auto button = root()->addWidget(Wt::cpp14::make_unique<Wt::WPushButton>("Enter"));
+  auto button = root()->addWidget(Wt::cpp14::make_unique<Wt::WPushButton>("ADD"));
                                                       // create a button
-  button->setMargin(100, Wt::Side::Left);                   // add 5 pixels margin
+  button->setMargin(5, Wt::Side::Left);                   // add 5 pixels margin
 
   root()->addWidget(Wt::cpp14::make_unique<Wt::WBreak>());    // insert a line break
   greeting_ = root()->addWidget(Wt::cpp14::make_unique<Wt::WText>()); // empty text
@@ -40,8 +41,7 @@ HelloApplication::HelloApplication(const Wt::WEnvironment& env)
 
 void HelloApplication::greet()
 {
-  /*
-   * Update the text, using text input into the nameEdit_ field.
-   */
-  greeting_->setText("Hi, " + nameEdit_->text() + ".");
+    int ans = std::stoi(nameEdit_->text());
+    sum += ans;
+    greeting_->setText("The total is " +  std::to_string(sum) + ".");
 }
--- addwt/HelloApplication.hpp
+++ addwt/HelloApplication.hpp
@@ -4,15 +4,31 @@
 #include <Wt/WLineEdit.h>
 #include <Wt/WPushButton.h>
 #include <Wt/WText.h>
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cin;
+using std::sort;
+using std::cout;
+using std::streamsize;
+using std::endl;
+using std::string;
+using std::setprecision;
+using std::vector;
+using std::stoi;
+;
 
 class HelloApplication: public Wt::WApplication
 {
 public:
     HelloApplication(const Wt::WEnvironment& env);
-
 private:
     Wt::WLineEdit *nameEdit_;
     Wt::WText     *greeting_;
-
+    int count = 0;
+    int sum = 0;
     void greet();
 };
