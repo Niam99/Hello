c86443d1b0d59ea793113aa3004a4b050e3cca25
Niam Islam <niamislam99@gmail.com>
2018-01-12 16:04:56 +0000

    added cpu ability to hit

--- twentyoneGame/game.cpp
+++ twentyoneGame/game.cpp
@@ -10,10 +10,14 @@ int main()
    int playercard2 = (rand() % 11) + 1;
    int cpucard1 = (rand() % 11) + 1;
    int cpucard2 = (rand() % 11) + 1;
+   int cpucard3 = (rand() % 11) + 1;
+   int cpucard4 = (rand() % 11) + 1;
+   int cpucard5 = (rand() % 11) + 1;
    int playertotal = playercard1 + playercard2;
    int cputotal = cpucard1 + cpucard2;
    int playerwin;
    int cpuwin;
+   int cpucardcount = 2;
    bool repeat = true;
 
    std::cout << "Your cards are" << std::endl;
@@ -45,17 +49,39 @@ int main()
        }
    }
 
-   std::cout << "The CPU's total is:" << std::endl;
+   //Decide how many cards the CPU draws
 
+   if (cputotal < 21)
+   {
+       cputotal = cputotal + cpucard3;
+       cpucardcount = 3;
+   }
+
+   if (cputotal < 21)
+   {
+       cputotal = cputotal + cpucard4;
+       cpucardcount = 4;
+   }
+
+   if (cputotal < 18)
+   {
+       cputotal = cputotal + cpucard5;
+       cpucardcount = 5;
+   }
+
+   std::cout << "The CPU had a total of: " << cpucardcount << " cards\n" ;
+   std::cout << "The CPUs total is:" << std::endl;
    std::cout << cputotal << std::endl;
-
    playerwin = 21 - playertotal;
    cpuwin = 21 - cputotal;
 
+
+   //decide who wins
    if (playerwin > cpuwin)
    {
        std::cout << "CPU wins!" << std::endl;
    }
+
    if (playerwin < cpuwin)
    {
        std::cout << "YOU WIN" << std::endl;
