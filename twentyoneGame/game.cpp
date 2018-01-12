24f1887bb1974153112f23e98d3950f779c1d3e3
Niam Islam <niamislam99@gmail.com>
2018-01-12 14:48:40 +0000

    added while loop

--- twentyoneGame/game.cpp
+++ twentyoneGame/game.cpp
@@ -14,55 +14,42 @@ int main()
    int cputotal = cpucard1 + cpucard2;
    int playerwin;
    int cpuwin;
+   bool repeat = true;
 
    std::cout << "Your cards are" << std::endl;
    std::cout << playercard1 << std::endl;
    std::cout << playercard2 << std::endl;
    std::cout << "Your total is:" << std::endl;
    std::cout << playertotal << std::endl;
-   std::cout << "The CPU's cards are" << std::endl;
+   /* std::cout << "The CPU's cards are" << std::endl;
    std::cout << cpucard1 << std::endl;
    std::cout << cpucard2 << std::endl;
    std::cout << "The CPU's total is" << std::endl;
+   std::cout << cputotal << std::endl;*/
+
+   while (repeat == true)
+   {
+       std::cout << "Do you want another card? 1-Y/2-N " << std::endl;
+       int hit;
+       std::cin >> hit;
+       if (hit == 1)
+       {
+           playertotal = playertotal + ((rand() % 11) + 1);
+           std::cout << "Your new total:" << std::endl;
+           std::cout << playertotal << std::endl;
+       }
+
+       if (hit == 2)
+       {
+           repeat = false;
+       }
+   }
+
+   std::cout << "The CPU's total is:" << std::endl;
+
    std::cout << cputotal << std::endl;
 
-   std::cout << "Do you want another card? 1-Y/2-N " << std::endl;
-   int hit;
-   std::cin >> hit;
-
-   if (hit == 1)
-   {
-       playertotal = playertotal + ((rand() % 11) + 1);
-       std::cout << "Your new total:" << std::endl;
-       std::cout << playertotal << std::endl;
-       playerwin = 21 - playertotal;
-       cpuwin = 21 - cputotal;
-       if (playerwin > cpuwin)
-       {
-           std::cout << "CPU wins!" << std::endl;
-       }
-       if (playerwin < cpuwin)
-       {
-           std::cout << "YOU WIN" << std::endl;
-       }
-   }
-
-   if (hit == 2)
-   {
-       playerwin = 21 - playertotal;
-       cpuwin = 21 - cputotal;
-
-       if (playerwin > cpuwin)
-       {
-           std::cout << "CPU wins!" << std::endl;
-       }
-
-       if (playerwin < cpuwin)
-       {
-           std::cout << "YOU WIN" <<std::endl;
-       }
-   }
-   /* playerwin = 21 - playertotal;
+   playerwin = 21 - playertotal;
    cpuwin = 21 - cputotal;
 
    if (playerwin > cpuwin)
@@ -72,7 +59,7 @@ int main()
    if (playerwin < cpuwin)
    {
        std::cout << "YOU WIN" << std::endl;
-       }*/
+   }
 
    return 0;
 }
