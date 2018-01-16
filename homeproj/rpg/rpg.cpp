f326f115243ebf7da12247b839f8505bc7eae7b1
Niam Islam <niamislam99@gmail.com>
2018-01-16 15:38:28 +0000

    update rpg

--- homeproj/rpg/rpg.cpp
+++ homeproj/rpg/rpg.cpp
@@ -12,6 +12,7 @@ public:
 int main()
 {
     bool fightBossPath = false;
+    bool fightBoss = false;
     bool fight = false;
     Character player;
     Character enemy1;
@@ -74,8 +75,8 @@ int main()
                 std::cout << "The enemy strikes back!!" << std::endl;
                 std::cout << "You lose " << enemy1.attack << " HP" << std::endl;
                 std::cout << "" << std::endl;
-                std::cout << "HP: " << player.health << " ST: " << player.stamina << std::endl;
                 player.health = player.health - enemy1.attack;
+                std::cout << "HP: " << player.health << " ST: " << player.stamina << std::endl;
 
             }
 
@@ -93,7 +94,8 @@ int main()
             {
                     std::cout << "YOU DEFEATED THE MONSTER!!!" << std::endl;
                     std::cout << "The monster's corpse vanishes revealing a golden sword!" << std::endl;
-                    std::cout << "You pick up this sword, increasing your attack power by 50 points!" << std::endl;
+                    std::cout << "You pick up this sword, increasing your attack power by 25 points!" << std::endl;
+                    player.attack = player.attack + 25;
                     fightBossPath = true;
                     fight = false;
             }
@@ -111,7 +113,68 @@ int main()
 
     if (choice1 == "rightpath" || fightBossPath == true)
     {
-        std::cout << "BOSS FIGHT" << std::endl;
-    }
+        std::cout << "You continue walking..." << std::endl;
+        std::cout << "........................." << std::endl;
+        std::cout << "A few hours pass" << std::endl;
+        std::cout << "You can see some light through the trees ahead!" << std::endl;
+        std::cout << "You walk towards the light and come to an opening" << std::endl;
+        std::cout << "The light is eminating from a pile of gold in the middle of an opening" << std::endl;
+        std::cout << "You walk towards the gold" << std::endl;
+        std::cout << "......................................................" << std::endl;
+        std::cout << "There is a loud rumbling noise!!!!!!" << std::endl;
+        std::cout << "A Dragon emerges from the shadows and stands in front of the gold!" << std::endl;
+        std::cout << "" << std::endl;
 
+        fightBoss = true;
+
+        do
+        {
+            std::cout << "What are you going to do?" << std::endl;
+            std::cout << "fight / run " << std::endl;
+            std::string fightchoice;
+            std::cin >> fightchoice;
+
+            if (fightchoice == "fight")
+            {
+                std::cout << "enemy HP: " << enemy1.health << std::endl;
+                std::cout << "You attacked the dragon!" << std::endl;
+                std::cout << "You dealt " << player.attack << " dmg" << std::endl;
+                boss.health = boss.health - player.attack;
+                std::cout << "enemy HP: " << boss.health << std::endl;
+                std::cout << "The dragon strikes back!!" << std::endl;
+                std::cout << "You lose " << boss.attack << " HP" << std::endl;
+                std::cout << "" << std::endl;
+                player.health = player.health - boss.attack;
+                std::cout << "HP: " << player.health << " ST: " << player.stamina << std::endl;
+
+            }
+
+            if (fightchoice == "run")
+            {
+                std::cout << "You look for a way to escape.." << std::endl;
+                std::cout << "The dragon attacks while you hesitate" << std::endl;
+                player.health = player.health - boss.attack;
+                std::cout << "HP: " << player.health << " ST: " << player.stamina << std::endl;
+                std:: cout << "There is nowhere to run, you have to fight!" << std::endl;
+                std::cout << "" << std::endl;
+            }
+
+            if (boss.health <= 0)
+            {
+                    std::cout << "YOU DEFEATED THE DRAGON!!!" << std::endl;
+                    std::cout << "The gold is yours :)" << std::endl;
+                    std::cout << "Congratulations " << pname << " , you have completed the game" << std::endl;
+                    exit(0);
+            }
+
+            if (player.health <= 0)
+            {
+                std::cout << "HP: " << player.health << std::endl;
+                std::cout << "You fall to the ground and everything begins to go dark..." << std::endl;
+                std::cout << "GAME OVER" << std::endl;
+                exit(0);
+            }
+
+        } while (fightBoss == true);
+    }
 }
