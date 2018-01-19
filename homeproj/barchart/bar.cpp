f712130149ee840de140af216840b296a4d44378
Niam Islam <niamislam99@gmail.com>
2018-01-19 11:06:03 +0000

    initial commit of barchart program

---
+++ homeproj/barchart/bar.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "BAR CHART PROGRAM" << std::endl;
+    std::cout << "" << std::endl;
+    std::string name1 = "Name1" ;
+    std::string name2 = "Name2" ;
+    std::string name3 = "Name3" ;
+    std::string name4 = "Name4" ;
+    std::string name5 = "Name5" ;
+
+    std::string name1length = "";
+    std::string name2length = "";
+    std::string name3length = "";
+    std::string name4length = "";
+    std::string name5length = "";
+
+    int var1 = 10;
+    int var2 = 20;
+    int var3 = 30;
+    int var4 = 40;
+    int var5 = 50;
+    int var6 = 60;
+    int var7 = 70;
+    int var8 = 80;
+    int var9 = 90;
+    int var10 = 100;
+
+    std::cout << "NAME - 0" << "|" << var1 << "|" << var2 << "|" << var3 << "|"
+              << var4 << "|" << var5 << "|" << var6 << "|" << var7 << "|"
+              << var8 << "|" << var9 << "|" << var10 << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << name1 << "|" << name1length << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << name2 << "|" << name2length << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << name3 << "|" << name3length << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << name4 << "|" << name4length << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+    std::cout << name5 << "|" << name5length << "|" << std::endl;
+    std::cout << "-----------------------------------------------" << std::endl;
+}
