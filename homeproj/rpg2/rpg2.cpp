8d748d815d3126de20fe2b17d4858b2661de1fc1
Niam Islam <niamislam99@gmail.com>
2018-01-22 15:46:01 +0000

    update rpg2

--- homeproj/rpg2/rpg2.cpp
+++ homeproj/rpg2/rpg2.cpp
@@ -12,6 +12,13 @@ public:
     int XP;
 };
 
+class Enemy {
+public:
+    std::string name;
+    int health;
+    int str;
+};
+
 
 int main()
 {
@@ -69,6 +76,86 @@ int main()
     std::cout << "" << std::endl;
     std::cout << "------------- ---------------- -------------- -------------- ----------" << std::endl;
     std::cout << "You put on your boots and exit the house..." << std::endl;
-    std::cout << "Where do you want to go? town / forest" << std::endl;
 
+    bool correctChoice = false;
+    do
+    {
+        std::string choice;
+        std::cout << "Where do you want to go? town / forest" << std::endl;
+        std::cin >> choice;
+        if (choice == "forest")
+        {
+            std::cout << "You walk towards the forest" << std::endl;
+            correctChoice = true;
+        }
+        else
+        {
+            std::cout << "Wait, Grandpa told me to go to the forest!" << std::endl;
+        }
+    } while (correctChoice == false);
+
+    std::cout << "You enter the forest through a large black gate" << std::endl;
+    std::cout << "--------------------------------------------------" << std::endl;
+    std::cout << "The forest is fairly big, however there aren't too many trees, so it is well lit" << std::endl;
+    std::cout << "You walk toward the usual spot, where there are plenty of vegetables..." << std::endl;
+    std::cout << "---------------------------------------------------" << std::endl;
+    std::cout << "You reach the usual spot, however there is a  man with a sword inspecting the vegetables!"
+              << std::endl;
+
+    Enemy bandit1;
+    bandit1.name = "bandit";
+    bandit1.health = 20;
+    bandit1.str = 5;
+
+    if (player1.intel > 8)
+    {
+        std::cout << "(intel) You realise that this man is clearly a bandit!!!" << std::endl;
+    }
+
+    bool fightState = true;
+
+    do
+    {
+        std::cout << "What would you like to do???" << std::endl;
+        std::cout << "| fight | talk |" << std::endl;
+        std::string choice;
+        std::cin >> choice;
+
+        if (choice == "fight")
+        {
+            std::cout << "HP: " << player1.health << std::endl;
+            std::cout << "enemy HP: " << bandit1.health << std::endl;
+            std::cout << "You attack the " << bandit1.name << " !" << std::endl;
+            std::cout << "You dealt " << player1.str << " dmg" << std::endl;
+            bandit1.health = bandit1.health - player1.str;
+            std::cout << "enemy HP: " << bandit1.health << std::endl;
+            std::cout << "The enemy strikes back!" << std::endl;
+            std::cout << "You lose " << bandit1.str << " HP" << std::endl;
+            player1.health = player1.health - bandit1.str;
+            std::cout << "HP: " << player1.health << std::endl;
+            std::cout << "----------------------------------------------" << std::endl;
+        }
+
+        if (choice == "talk")
+        {
+            std::cout << "You can't reason with a bandit!!!" << std::endl;
+        }
+
+        if (bandit1.health <= 0)
+        {
+            std::cout << "You defeated the bandit!!" << std::endl;
+            std::cout << "The injured bandit runs off" << std::endl;
+            fightState = false;
+        }
+
+        if (player1.health <= 0)
+        {
+            std::cout << "HP: " << player1.health << std::endl;
+            std::cout << "You fall to the ground and everything begins to go dark..." << std::endl;
+            std::cout << "GAME OVER" << std::endl;
+            exit(0);
+        }
+    } while (fightState == true);
+
+    std::cout << "--------------------------------" << std::endl;
 }
