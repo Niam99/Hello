d92e0d2fe266c794f647f14e38b78c6575305c50
Niam Islam <niamislam99@gmail.com>
2018-01-22 14:31:05 +0000

    rpg intro

--- homeproj/rpg2/rpg2.cpp
+++ homeproj/rpg2/rpg2.cpp
@@ -58,4 +58,17 @@ int main()
             std::cout << "TRY AGAIN" << std::endl;
         }
     } while (statSet == false);
+
+    std::cout << "????: " << "WAKE UP KID!!!!!!" << std::endl;
+    std::cout << "You: " << "Huh....." << std::endl;
+    std::cout << "You get up from bed and look at the man shouting at you..." << std::endl;
+    std::cout << "It's Grandpa..... he took you in when you were little..." << std::endl;
+    std::cout << "Grandpa: " << "Hey, I need you to go and get some VEGGIES from the FOREST" << std::endl;
+    std::cout << "Grandpa: " << "Don't take too long, I need to open up shop soon" << std::endl;
+    std::cout << "Grandpa leaves the room..." << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "------------- ---------------- -------------- -------------- ----------" << std::endl;
+    std::cout << "You put on your boots and exit the house..." << std::endl;
+    std::cout << "Where do you want to go? town / forest" << std::endl;
+
 }
